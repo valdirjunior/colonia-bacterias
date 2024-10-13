980cfce17bdd5cd007579348bae4f4b4f5ceba23
valdirjunior <valdirrugiskijr@gmail.com>
2024-10-13 00:12:06 -0300

    ajustado thread_create

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 #include <stdlib.h>
 #include <semaphore.h>
 
-#define NUM_THREADS 4
-#define NUM_RECURSOS 4
+#define NUM_THREADS 8
+#define NUM_RECURSOS 8
 
 typedef struct {
+    int threadNum;
     int tipoColonia;
     double popInicial;
     double txCrescimento;
@@ -34,29 +35,30 @@ int main() {
     sem_init(&espaco, 0, NUM_RECURSOS);
 
     ThreadArgs *threadArgs = malloc(sizeof(ThreadArgs) * NUM_THREADS);
-    pthread_t **threads = malloc(sizeof(pthread_t*) * NUM_THREADS);
+    pthread_t threads[NUM_THREADS];
 
     for(int i = 0; i < NUM_THREADS; i++) {
-        int tipo = rand() % 10;
+        int tipo = rand() % 2;
         if(tipo == 1) {
             threadArgs[i].tipoColonia = 2;
         } else {
             threadArgs[i].tipoColonia = 1;
         }
+        threadArgs[i].threadNum = i + 1;
         threadArgs[i].popInicial = 100;
-        threadArgs[i].txCrescimento = 0.002;
-        threadArgs[i].tempoTotal = 30;
+        threadArgs[i].txCrescimento = 0.2;
+        threadArgs[i].tempoTotal = 300;
         threadArgs[i].tempoAtual = 1;
         threadArgs[i].popAtual = 100;
         threadArgs[i].alimento = &alimento;
         threadArgs[i].espaco = &espaco;
         threadArgs[i].tempoDecorrido = 0;
 
-        pthread_create(threads[i], NULL, threadFunction, &threadArgs[i]);
+        pthread_create(&threads[i], NULL, threadFunction, &threadArgs[i]);
     }
 
     for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(*threads[i], NULL);
+        pthread_join(threads[i], NULL);
     }
 
     return 0;
@@ -68,12 +70,22 @@ void* threadFunction(void *args) {
     ThreadArgs *threadArgs = (ThreadArgs *)args;
 
     while (threadArgs->popAtual <= threadArgs->tempoTotal) {
-        sleep(rand() %5);
+        sleep(rand() % 5);
 
-        sem_wait(threadArgs->espaco);
-        sem_wait(threadArgs->alimento);
+        if(threadArgs->tipoColonia == 1) {
+            sem_wait(threadArgs->alimento);
+        } else {
+            sem_wait(threadArgs->espaco);
+        }
 
-        threadArgs->popAtual = threadArgs->tempoAtual * (1 + threadArgs->txCrescimento);
+        if(threadArgs->tipoColonia == 1) {
+            sem_wait(threadArgs->espaco);
+        } else {
+            sem_wait(threadArgs->alimento);
+        }
+
+
+        threadArgs->popAtual = threadArgs->popAtual * (1 + threadArgs->txCrescimento);
 
         sem_post(threadArgs->alimento);
         sem_post(threadArgs->espaco);
@@ -81,10 +93,12 @@ void* threadFunction(void *args) {
     }
     threadArgs->tempoDecorrido = difftime(time(NULL), tempoInicio);
 
+    printf("Thread: %d\n", threadArgs->threadNum);
     printf("Tipo da colonia: %d\n", threadArgs->tipoColonia);
-    printf("População inicial: %d\n", threadArgs->popInicial);
-    printf("Taxa de crescimento: %d\n", threadArgs->txCrescimento);
+    printf("População inicial: %g\n", threadArgs->popInicial);
+    printf("Taxa de crescimento: %g\n", threadArgs->txCrescimento);
     printf("Tempo: %d\n", threadArgs->tempoTotal);
-    printf("População final: %d\n", threadArgs->popAtual);
-    printf("Tempo decorrido: %d\n", threadArgs->tempoDecorrido);
+    printf("População final: %g\n", threadArgs->popAtual);
+    printf("Tempo decorrido: %gs\n", threadArgs->tempoDecorrido);
+    printf("\n\n");
 }
